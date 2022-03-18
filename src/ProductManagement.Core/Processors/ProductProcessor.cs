e20545522949f3ca6bfe78d3a60d08344fe79d7e
Danfer Habed <dhabed@outlook.com>
2022-03-18 05:34:22 -0600

    Mocking productservice, testing

--- src/ProductManagement.Core/Processors/ProductProcessor.cs
+++ src/ProductManagement.Core/Processors/ProductProcessor.cs
@@ -1,4 +1,5 @@
 ﻿using ProductManagement.Core.DataServices;
+using ProductManagement.Core.Domain;
 using ProductManagement.Core.Models;
 using System;
 using System.Collections.Generic;
@@ -10,14 +11,24 @@ namespace ProductManagement.Core.Processors
 {
     public class ProductProcessor
     {
-        public ProductProcessor(IProductService @object)
+        private readonly IProductService _productService;
+        public ProductProcessor(IProductService productService)
         {
+            _productService = productService;
         }
 
         public ProductResult AddProduct(ProductRequest productRequest)
         {
             if (productRequest is null)
                 throw new ArgumentNullException(nameof(productRequest));
+
+            _productService.Save(new Product
+            {
+                ProductId = productRequest.ProductId,
+                ProductName = productRequest.ProductName,
+                CategoryId = productRequest.CategoryId,
+                CategoryName = productRequest.CategoryName,
+            });
 
             return new ProductResult
             {
